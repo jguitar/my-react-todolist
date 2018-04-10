f6ddb7c5bbbe544e75d20f3caaa4be1789d377dd
Juan F. Pérez <j.guitar@gmail.com>
2018-04-10 14:22:07 +0200

    add AddTodo to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
+import AddTodo from './components/addTodo/';
 
 const App = () => (
-  <div>My App</div>
+  <div>
+    <h1>Todo List</h1>
+    <AddTodo />
+  </div>
 );
 
 export default App;
