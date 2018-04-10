ae6f8e556e04ffa9d3867ef3a13ce4462e91fddc
Juan F. Pérez <j.guitar@gmail.com>
2018-04-10 16:48:57 +0200

    add a complex reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,26 @@
-export const initialState = {};
+import types from '../constants';
 
-export const reducer = (state = initialState, action) => state;
+export const initialState = {
+  todos: [],
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.SUBMIT_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: action.id,
+            text: action.text,
+          },
+        ],
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
