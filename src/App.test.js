bfcdf6e9eb12d5eb8c9b6b3c89325ae999b9e294
Juan F. Pérez <j.guitar@gmail.com>
2018-04-10 14:09:51 +0200

    setup tests for Enzyme and React 16

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 /* global it, expect */
 import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import App from './App';
-
-configure({ adapter: new Adapter() });
 
 it('App renders without crashing', () => {
   const component = shallow(<App />);
