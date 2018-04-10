0bfa7437edb94173bf31cd413a859c7ae639eecd
Juan F. Pérez <j.guitar@gmail.com>
2018-04-10 11:32:48 +0200

    add enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
+/* global it, expect */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
