be1012c97211009ac97d6968b579c2c3b3a9c717
Juan F. Pérez <j.guitar@gmail.com>
2018-04-16 17:03:52 +0200

    format with standard

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+// import { action } from '@storybook/addon-actions'
+// import { linkTo } from '@storybook/addon-links'
 
 import Clock from '../components/clock'
 import Page from '../components/page/'
 import Toggle from '../components/toggle'
 
-storiesOf('Clock', module).add('sample', () => <Clock />);
+storiesOf('Clock', module).add('sample', () => <Clock />)
 
-storiesOf('Page', module).add('sample', () => <Page />);
+storiesOf('Page', module).add('sample', () => <Page />)
 
-storiesOf('Toggle', module).add('sample', () => <Toggle />);
+storiesOf('Toggle', module).add('sample', () => <Toggle />)
